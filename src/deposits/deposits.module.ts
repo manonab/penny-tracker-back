0e63e8c1d9cc47cc8c2aacf50a9ff7b010b3faf6
manonab <manon.abel-coindoz@hostnfly.com>
2024-08-23 15:21:25 +0200

    finnalyy fixed errors

--- src/deposits/deposits.module.ts
+++ src/deposits/deposits.module.ts
@@ -3,8 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Deposit } from './deposits.entity';
 import { DepositService } from './deposits.service';
 import { DepositController } from './deposits.controller';
+import { User } from 'src/users/user.entity';
+import { Couple } from 'src/couple/couple.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Deposit])],
+  imports: [TypeOrmModule.forFeature([Deposit, User, Couple])],
   providers: [DepositService],
   controllers: [DepositController],
   exports: [DepositService],
