9359d519889cb11e3f8e1f89ec583dbec518cdde
manonab <manon.abel-coindoz@hostnfly.com>
2024-08-24 19:17:27 +0200

    added netlify at cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://penny-tracker-back.onrender.com',
-    ],
+    origin: ['http://localhost:3000', 'https://penny-tracker.netlify.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
